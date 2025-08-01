96bb76392cbcfd4bd49bbe1a4e648c081e2f48ac
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2025-08-01 19:43:24 +0330

    DjangoTemplateEngine has been used

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-from django.conf import settings
-import django
-from django.template import Context, Template
-
-# mark_safe is for mixixng proccessed markdown with django template
-import django.utils.safestring
-
 from processor.markdown_processor import MarkdownProcessor
 from core.page import Page
 from utils.fs_manager import FileSystemManager
-
+from engines.django_engine import DjangoTemplateEngine
 
 # TODO: Remove this, this was just a test
 # import sys
@@ -20,26 +13,6 @@ from utils.fs_manager import FileSystemManager
 #     f"{GREEN}Converting process has been done succefuly{RESET}",
 #     file=sys.stderr,
 # )
-
-# --- Crucial Setup for Standalone DTL Usage ---
-if not settings.configured:
-    settings.configure(
-        TEMPLATES=[  # This is the key setting
-            {
-                "BACKEND": "django.template.backends.django.DjangoTemplates",
-                # 'DIRS': [],  # For file-based templates, list of directories
-                # 'APP_DIRS': False, # Usually True in projects, False for simple standalone
-                "OPTIONS": {
-                    # 'debug': True, # Optional: for more detailed error messages during dev
-                },
-            }
-        ]
-        # You might need to add other minimal settings here if your template
-        # uses features that depend on them, e.g., INSTALLED_APPS for some tags.
-        # For basic rendering as in the example, TEMPLATES is usually enough.
-    )
-    django.setup()  # This initializes Django's settings and application registry
-# --- End of Setup ---
 
 fs_handler = FileSystemManager()
 md_processor = MarkdownProcessor(["extra", "meta", "codehilite"])
@@ -95,15 +68,9 @@ template_string = """
 </html>
 """
 
-# 2. Create a Template object
-t = Template(template_string)
-
-# 3. Define the context data
+template_engine = DjangoTemplateEngine()
 context_data = {"page_title": test_page.get_title(), "content": test_page.get_contex()}
-c = Context(context_data)
-
-# 4. Render the template with the context
-rendered_html = t.render(c)
+rendered_html = template_engine.render_from_string(template_string, context_data)
 
 # 5. Print the output
 print(rendered_html)

7d509f664dc4738b406056d693c06a8538a9bec6
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2025-08-12 16:29:39 +0330

    doc string has been added

--- engines/base_engine.py
+++ engines/base_engine.py
@@ -3,15 +3,48 @@ from django.template import Template
 
 
 class TemplateEngine(ABC):
-    # TODO: Write doc strings for this
+    """
+    Abstract base class for template engines.
+    Defines the interface for rendering templates and loading them.
+    """
+
     @abstractmethod
     def render(self, template_name: str, context: dict) -> str:
+        """
+        Render a template by name with the given context.
+
+        Args:
+            template_name (str): The name of the template to render.
+            context (dict): The context data to use for rendering.
+
+        Returns:
+            str: The rendered template as a string.
+        """
         pass
 
     @abstractmethod
     def render_from_string(self, template_string: str, context: dict) -> str:
+        """
+        Render a template from a string with the given context.
+
+        Args:
+            template_string (str): The template content as a string.
+            context (dict): The context data to use for rendering.
+
+        Returns:
+            str: The rendered template as a string.
+        """
         pass
 
     @abstractmethod
     def load_template(self, template_name: str) -> Template | str:
+        """
+        Load a template by name.
+
+        Args:
+            template_name (str): The name of the template to load.
+
+        Returns:
+            Template | str: The loaded template object or its content as a string.
+        """
         pass
--- engines/django_engine.py
+++ engines/django_engine.py
@@ -2,17 +2,18 @@ from django.conf import settings
 import django
 from django.template import Context, Template
 
-# mark_safe is for mixixng proccessed markdown with django template
+# mark_safe is for mixing processed markdown with django template
 import django.utils.safestring
 
 from .base_engine import TemplateEngine
 from utils.fs_manager import FileSystemManager
 
 # TODO: Clean this file.
-# TODO: Add doc string.
 
 
 class DjangoTemplateEngine(TemplateEngine):
+    """A template engine that uses Django's template system for rendering HTML templates."""
+
     def __init__(self) -> None:
         # --- Crucial Setup for Standalone DTL Usage ---
         if not settings.configured:
@@ -28,24 +29,56 @@ class DjangoTemplateEngine(TemplateEngine):
         # --- End of Setup ---
 
     def render(self, template_name: str, context: dict) -> str:
+        """
+        Renders a template by name with the given context.
+
+        Args:
+            template_name (str): The name of the template file to render.
+            context (dict): The context data to pass to the template.
+
+        Returns:
+            str: The rendered HTML as a string.
+        """
         template = self.load_template(template_name)
         context_obj = Context(context)
         rendered_html = template.render(context_obj)
         return rendered_html
 
     def render_from_string(self, template_string: str, context: dict) -> str:
+        """
+        Renders a template from a string with the given context.
+
+        Args:
+            template_string (str): The template content as a string.
+            context (dict): The context data to pass to the template.
+
+        Returns:
+            str: The rendered HTML as a string.
+        """
         template = Template(template_string)
         context_obj = Context(context)
         rendered_html = template.render(context_obj)
         return rendered_html
 
     def load_template(self, template_name: str) -> Template:
+        """
+        Loads a template by name from the templates directory.
+
+        Args:
+            template_name (str): The name of the template file to load.
+
+        Returns:
+            Template: A Django Template object loaded with the template content.
+        """
+
         fs_handler = FileSystemManager()
         templates = fs_handler.list_files("./templates", recursive=True)
         found = False
         # TODO: change that in future to something like default.html
+        # Setting a default template for fallback
         template_file = fs_handler.read_file("./templates/blog-theme/post.html")
         for template in templates:
+            # If template name found in template path
             if template_name in template:
                 found = True
                 template_file = fs_handler.read_file(template)

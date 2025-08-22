e6d0d1c764074da3897e1b7585551c786e6dcc58
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2025-08-22 22:04:44 +0330

    File has been modified two use new files and libraries

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from processor.markdown_processor import MarkdownProcessor
-from core.page import Page
-from utils.fs_manager import FileSystemManager
-from engines.django_engine import DjangoTemplateEngine
 from core.config import Config
+from core.project import Project
+
 
 import logging
 import argparse
@@ -46,24 +44,36 @@ debug = app_config.get("debug", True)
 logging.basicConfig(
     # TODO: Fix this
     level=logging.DEBUG if debug else logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    format="%(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 logging.debug("Debug mode is active")
 logging.info("Initilizing new logger")
 
-fs_handler = FileSystemManager()
-md_processor = MarkdownProcessor(["extra", "meta", "codehilite"])
-test_page = Page()
-test_page.read_source_file("main.md")
-test_page.process_content(md_processor)
-test_page.process_metadata(md_processor)
+# fs_handler = FileSystemManager()
+# md_processor = MarkdownProcessor(["extra", "meta", "codehilite"])
+# test_page = Page("source/main.md")
+# test_page.read_source_file()
+# test_page.process_content(md_processor)
+# test_page.process_metadata(md_processor)
+# test_page.set_page_type()
+# print(test_page.get_page_type())
 
 
-template_engine = DjangoTemplateEngine()
-context_data = {"page_title": test_page.get_title(), "content": test_page.get_contex()}
-rendered_html = template_engine.render("post", context_data)
+# template_engine = DjangoTemplateEngine()
+# context_data = {"page_title": test_page.get_title(), "content": test_page.get_contex()}
+# rendered_html = template_engine.render("post", context_data)
 
 
-fs_handler.write_file("output/output.html", rendered_html)
-fs_handler.copy_directory("styles", "output/styles")
+# fs_handler.write_file("output/output.html", rendered_html)
+# fs_handler.copy_directory("styles", "output/styles")
+
+
+def main():
+    # Assume the config is in the current directory
+    my_project = Project("_config.yaml")
+    my_project.build()
+
+
+if __name__ == "__main__":
+    main()

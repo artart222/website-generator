1d59289e991ae8581a38c8b4425a7011978ca5f0
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2025-08-31 20:39:49 +0330

    Some tweaks to make plugin ultimate flexible

--- plugins/base_plugin.py
+++ plugins/base_plugin.py
@@ -1,39 +1,36 @@
 from abc import ABC, abstractmethod
-from typing import Optional
-from core.page import Page
-from core.config import Config
-from core.site import Site
+from typing import Any
 
 
 class BasePlugin(ABC):
     """Base class for all plugins."""
 
     @abstractmethod
-    def on_config_loaded(self, config: Config, site: Site) -> None:
+    def on_config_loaded(self, *args, **kwargs) -> Any:
         """
         Called after the configuration file is loaded.
         """
 
     @abstractmethod
-    def on_before_build(self, site: Site) -> None:
+    def on_before_build(self, *args, **kwargs) -> Any:
         """
         Called before the site build starts.
         """
 
     @abstractmethod
-    def on_page_parsed(self, page: Page, site: Site) -> None:
+    def on_page_parsed(self, *args, **kwargs) -> Any:
         """
         Called after a Page object is created and parsed.
         """
 
     @abstractmethod
-    def on_page_rendered(self, page: Optional[Page], site: Site) -> None:
+    def on_page_rendered(self, *args, **kwargs) -> Any:
         """
         Called after a Page is rendered into HTML.
         """
 
     @abstractmethod
-    def on_after_build(self, site: Site) -> None:
+    def on_after_build(self, *args, **kwargs) -> Any:
         """
         Called after the whole site is built.
         """

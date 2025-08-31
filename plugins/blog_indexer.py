c31be8674ed02413aee352fbdd1276020b00d985
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2025-08-31 20:40:42 +0330

    Fix small blog_indexer issues

--- plugins/blog_indexer.py
+++ plugins/blog_indexer.py
@@ -1,41 +1,69 @@
 from core.page import Page
 from core.site import Site
 from .base_plugin import BasePlugin
-from typing import Optional
+import logging
 
 
 class BlogIndexerPlugin(BasePlugin):
-    def on_config_loaded(self, config, site) -> None:
-        return super().on_config_loaded(config, site)
+    """
+    Plugin that generates a blog index page listing all pages
+    whose type contains 'blog'. Adds the index page to the site after build.
+    """
 
-    def on_before_build(self, site) -> None:
-        return super().on_before_build(site)
+    def __init__(self) -> None:
+        self.logger = logging.getLogger(__name__)
 
-    def on_after_build(self, site) -> None:
+    def on_config_loaded(self, **kwargs):
+        """Nothing to do during config loading."""
+        pass
+
+    def on_before_build(self, **kwargs):
+        """Nothing to do before build."""
+        pass
+
+    def on_after_build(self, **kwargs) -> None:
+        """
+         Generate the blog index page after all pages are built.
+
+        Expected kwargs:
+            site (Site): The Site object
+        """
+        site = kwargs.get("site")
+        msg = "Missing or invalid 'site' argument in BlogIndexerPlugin.on_after_build"
+        self.logger.warning(msg)
+        if not isinstance(site, Site):
+            raise ValueError(msg)
+
         blog_pages = [
             p
             for p in site.get_pages()
             if p.get_page_type() is not None and "blog" in p.get_page_type()
         ]
 
+        self.logger.debug(f"Detected blog pages: {blog_pages}")
+
         # Build simple HTML list
-        items = "\n".join(
-            f"<li><article><a href='{p.get_output_path()}'>{p.title}</a></article></li>"
+        list_items = "\n".join(
+            f"<li><article><a href='{'../' + '/'.join(p.get_output_path().split('/')[1:])}'>{p.title}</a></article></li>"
             for p in blog_pages
         )
+        html_list = f"<ul>{list_items}</ul>"
 
         # Create a new Page object
+        self.logger.debug("Generating virtual index page")
         index_page = Page(source_filepath="", config=None, fs_manager=None)
         index_page.add_metadata({"template": "blog-indexer.html"})
         index_page.set_output_path("./output/blog-indexer/blog-indexer.html")
-        index_page.set_processed_content(items)
+        index_page.set_processed_content(html_list)
 
         # Add to site pages
         site.add_page(index_page)
-        site.logger.info("Blog index page generated.")
+        site.logger.debug("Blog index page generated.")
 
-    def on_page_parsed(self, page: Page, site: Site) -> None:
+    def on_page_parsed(self, **kwargs):
+        """No action needed for page parsed."""
         pass
 
-    def on_page_rendered(self, page: Optional[Page], site: Site) -> None:
-        return super().on_page_rendered(page, site)
+    def on_page_rendered(self, **kwargs):
+        """No action needed for page rendered."""
+        pass
